f75c2e19d8559a7cf05b98b1753b5e5ad41fb113
Jerry <jerryzhu96@hotmail.com>
2018-05-30 22:45:54 +1000

    a

--- dns_compiling.py
+++ dns_compiling.py
@@ -24,7 +24,7 @@ class TestSwitch(app_manager.RyuApp):
 
     def __init__(self, *args, **kwargs):
         super(TestSwitch, self).__init__(*args, **kwargs)
-        self.monitor_thread = hubspawn
+        # self.monitor_thread = hub.spawn(self.flow_request())
 
     def add_flow(self, datapath, priority, match, actions):
         ofproto = datapath.ofproto
@@ -107,15 +107,31 @@ class TestSwitch(app_manager.RyuApp):
 
     def flow_request(self):
         """Requests flows in a separate thread"""
-        datapath = dp
-        ofproto = datapath.ofproto
+        while True:
+            datapath = self.dp
+            if datapath is not None:
+                break
+
+        ofp = datapath.ofproto
         parser = datapath.ofproto_parser
 
         while True:
-            match = parser.OFPMatchs(ipv4_dst=ip_address, eth_type=0x0800, udp_dst=53, ip_proto=17)
+            # Get flow stat for flow entry of DNS reply
+
+            match = parser.OFPMatch(ipv4_dst="1.0.0.1", eth_type=0x0800, udp_src=53, ip_proto=17)
             # Not sure about this?
             stats = parser.OFPFlowStatsRequest(dp, 0, ofp.OFPP_ALL, ofp.OFPP_ANY, 0, 0, match)
             datapath.send_msg(stats)
+
+            # Get flow state for flow entry of DNS request
+            match = parser.OFPMatch(ipv4_src="1.0.0.1", eth_type=0x0800, udp_dst=53, ip_proto=17)
+            # Not sure about this?
+            stats = parser.OFPFlowStatsRequest(dp, 0, ofp.OFPP_ALL, ofp.OFPP_ANY, 0, 0, match)
+            datapath.send_msg(stats)
+
+            hub.sleep(10)
+
+
 
             # Sleep the thread
 
@@ -126,7 +142,7 @@ class TestSwitch(app_manager.RyuApp):
         self.logger.info("Print out dns request info")
         for stats in ev.msg.body:
             actions = ofctl_v1_3.actions_to_str(stats.actions)
-            match = ofctl_v1_4.match_to_str(stat.match)
+            match = ofctl_v1_3.match_to_str(stat.match)
             print {'dpid': ev.msg.datapath.id,
                    'priority': stats.priority,
                    'cookie': stats.cookie,
