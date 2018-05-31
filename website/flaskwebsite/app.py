2993f37f1eae4d0de6d082cd3086657b357ef242
Jerry <jerryzhu96@hotmail.com>
2018-05-31 21:13:34 +1000

    Added tables

--- website/flaskwebsite/app.py
+++ website/flaskwebsite/app.py
@@ -15,22 +15,27 @@ def index():
     # pie_chart.add('Opera', 2.3)
     # pie_chart.render()
     # graph_data = pie_chart.render_data_uri()
-    graph_data = createPie("dl_usage.txt", 'Download Usage')
-    up_data = createPie("up_usage.txt", 'Upload Usage')
-    tot_data = createPie("tot_usage.txt", 'Total Usage')
-    return render_template("home.html", graph_data = graph_data, up_data = up_data, tot_data = tot_data)
+    graph_data, down_list = createPie("dl_usage.txt", 'Download Usage')
+    up_data, up_list = createPie("up_usage.txt", 'Upload Usage')
+    tot_data, tot_list = createPie("tot_usage.txt", 'Total Usage')
+    return render_template("home.html", graph_data = graph_data, up_data = up_data, tot_data = tot_data, uploads = up_list, downloads = down_list, total = tot_list)
 
 def createPie(filename, title):
+    list = []
     pie = pygal.Pie(half_pie = True)
     pie.title= title
     total = 0
 
     with open(filename, 'r') as f:
+        count = 1
         for line in f:
             parts = line.split(" ")
             pie.add(parts[0], int(parts[1]))
+            dict = {'count': str(count), 'proto': parts[0], 'bytes': parts[1]}
+            count += 1
+            list.append(dict)
     pie.render()
-    return pie.render_data_uri()
+    return pie.render_data_uri(), list
 
 
 
